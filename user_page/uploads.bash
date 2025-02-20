b1b80f05bf26626505636365626139702b49be21
Snehasish Sarkar <snehasishsarkar439@gmail.com>
2025-02-20 22:26:39 +0530

    Delete user_page directory

--- user_page/uploads.bash
+++
@@ -1,9 +0,0 @@
-mkdir uploads
-chmod 755 uploads
-composer --version
-mysql -u root -p
-chmod 755 uploads/
-chmod 0777 uploads/documents/
-chmod 777 profile_pics/uploads/7c2a0207-ca67-4a1d-a755-abd3f7ab7d7b.png
-chmod 755 profile_pics
-sass home.scss home.css
