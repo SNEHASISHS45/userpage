925e9ce9273f5e125e52387caec0a8b428a10d89
Snehasish Sarkar <snehasishsarkar439@gmail.com>
2025-03-19 21:39:13 +0530

    f

--- config.php
+++ config.php
@@ -1,9 +1,16 @@
 <?php
-$host = getenv('DB_HOST'); // DB_HOST should be set to 'dpg-cvde525svqrc73efsaeg-a.oregon-postgres.render.com'
-$port = getenv('DB_PORT'); // DB_PORT should be set to '5432'
-$dbname = getenv('DB_NAME'); // DB_NAME should be set to 'sdrive'
-$user = getenv('DB_USER'); // DB_USER should be set to 'sdrive_user'
-$password = getenv('DB_PASSWORD'); // DB_PASSWORD should be set to 'VmuIcCKW8MUIUrYz8GFxIdLzTMKlbuRh'
+var_dump(getenv('DB_HOST'));
+var_dump(getenv('DB_PORT'));
+var_dump(getenv('DB_NAME'));
+var_dump(getenv('DB_USER'));
+var_dump(getenv('DB_PASSWORD'));
+
+
+$host = 'dpg-cvde525svqrc73efsaeg-a.oregon-postgres.render.com';
+$port = '5432';
+$dbname = 'sdrive';
+$user = 'sdrive_user';
+$password = 'VmuIcCKW8MUIUrYz8GFxIdLzTMKlbuRh'; // Ensure correct password
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
@@ -11,4 +18,5 @@ if (!$conn) {
     die("Connection failed: " . pg_last_error());
 }
 echo "Connected successfully to PostgreSQL!";
+
 ?>
