dc717e6e8787784ef15fc5d80c7d5d5d1c3229c1
Snehasish Sarkar <snehasishsarkar439@gmail.com>
2025-03-19 23:14:27 +0530

    f

--- config.php
+++ config.php
@@ -1,9 +1,4 @@
 <?php
-require 'vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
 $host = getenv('DB_HOST');
 $port = getenv('DB_PORT');
 $dbname = getenv('DB_NAME');
