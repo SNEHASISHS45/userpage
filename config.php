e4befaf4f47a8e50c98f007e1289eb30ef49d71b
Snehasish Sarkar <snehasishsarkar439@gmail.com>
2025-03-19 22:00:40 +0530

    f

--- config.php
+++ config.php
@@ -1,21 +1,14 @@
 <?php
-require 'vendor/autoload.php';
+$host = 'dpg-cvde525svqrc73efsaeg-a.oregon-postgres.render.com';
+$port = '5432';
+$dbname = 'sdrive';
+$user = 'sdrive_user';
+$password = 'VmuIcCKW8MUIUrYz8GFxIdLzTMKlbuRh';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-$host = getenv('DB_HOST');
-$port = getenv('DB_PORT');
-$dbname = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
-
-// Connect to PostgreSQL
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 if (!$conn) {
     die("Connection failed: " . pg_last_error());
 }
-
 echo "Connected successfully to PostgreSQL!";
 ?>
