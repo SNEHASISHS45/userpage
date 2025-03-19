293546b9fc92199d50f3b38279197e43c8095bd3
Snehasish Sarkar <snehasishsarkar439@gmail.com>
2025-03-19 21:33:10 +0530

    f

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
-$host = getenv('dpg-cvde525svqrc73efsaeg-a.oregon-postgres.render.com');
-$port = getenv('5432');
-$dbname = getenv('sdrive');
-$user = getenv('sdrive_user');
-$password = getenv('VmuIcCKW8MUIUrYz8GFxIdLzTMKlbuRh');
+$host = getenv('DB_HOST'); // DB_HOST should be set to 'dpg-cvde525svqrc73efsaeg-a.oregon-postgres.render.com'
+$port = getenv('DB_PORT'); // DB_PORT should be set to '5432'
+$dbname = getenv('DB_NAME'); // DB_NAME should be set to 'sdrive'
+$user = getenv('DB_USER'); // DB_USER should be set to 'sdrive_user'
+$password = getenv('DB_PASSWORD'); // DB_PASSWORD should be set to 'VmuIcCKW8MUIUrYz8GFxIdLzTMKlbuRh'
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
